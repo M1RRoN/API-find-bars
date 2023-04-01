0753cc12e57857d7ad19e7ff65fc652165ef88c7
M1RRoN <mds.myst@gmail.com>
2023-04-01 13:36:48 +0400

    add review serializer

--- api_bars/urls.py
+++ api_bars/urls.py
@@ -15,6 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from categories.views import CategoryViewSet
+from establishments.views import PlaceViewSet
+
+router = DefaultRouter()
+router.register(r'places', PlaceViewSet, basename='place')
+router.register(r'reviews', ReviewViewSet, basename='review')
+router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
