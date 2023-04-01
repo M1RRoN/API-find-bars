4b1e391576248fa04632790aaf263cb45065dde7
M1RRoN <mds.myst@gmail.com>
2023-04-01 13:38:24 +0400

    add urls

--- api_bars/urls.py
+++ api_bars/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from categories.views import CategoryViewSet
 from establishments.views import PlaceViewSet
+from reviews.views import ReviewViewSet
 
 router = DefaultRouter()
 router.register(r'places', PlaceViewSet, basename='place')
@@ -27,4 +28,5 @@ router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
